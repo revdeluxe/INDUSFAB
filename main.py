72940446f3bf832ecbdaa52c080b157788154ee9
Richard Villareal <38414584+revdeluxe@users.noreply.github.com>
2025-04-07 15:53:50 +0800

    v0.01

---
+++ main.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, world!")
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
